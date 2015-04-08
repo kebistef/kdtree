78a26ae48abe912c1cb3d1e3b069e914754c95b3
jasmin <jasminstefanussen@gmail.com>
2015-04-07 23:11:01 -0600

    progress

--- src/a05/KdTreeST.java
+++ src/a05/KdTreeST.java
@@ -1,6 +1,10 @@
 package a05;
 
-import java.awt.RenderingHints.Key;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.Queue;
+
 
 /**
  * @author Kyle Ottman
@@ -17,13 +21,12 @@ public class KdTreeST<Value> {
 	private int N;			//size
 	
 	private class Node {
-		private Key key;
 		private Value val;
-		private Node lb;
-		private Node rb;
+		private Node leftOrBottom;
+		private Node rightOrTop;
 		private RectHV rect;
 		private Point2D p;
-		private int N;
+//		private int N;
 		
 		public Node(Point2D point, Value value, RectHV rectangle)
 		{
@@ -54,13 +57,13 @@ public class KdTreeST<Value> {
 	 * @return int size
 	 */
 	public int size() {
-		return size(root);
+		return N;
 	}
 
-	private int size(Node x) {
-		if (x == null) return 0;
-		return x.N;
-	}
+//	private int size(Node x) {
+//		if (x == null) return 0;
+//		return x.N;
+//	}
 	
 	/**
 	 * associate the value val with point p
@@ -92,16 +95,16 @@ public class KdTreeST<Value> {
 		Point2D parentNode = node.p;
 
 		if (orient && parentNode.x() > point.x()) { // Horizontal left
-			node.lb = put(node.lb, point, val, !orient, new RectHV(rect.xmin(), rect.ymin(), parentNode.x(), rect.ymax()));
+			node.leftOrBottom = put(node.leftOrBottom, point, val, !orient, new RectHV(rect.xmin(), rect.ymin(), parentNode.x(), rect.ymax()));
 		}
 		else if (orient) { // Horizontal right
-			node.rb = put(node.rb, point, val, !orient, new RectHV(parentNode.x(), rect.ymin(), rect.xmax(), rect.ymax()));
+			node.rightOrTop = put(node.rightOrTop, point, val, !orient, new RectHV(parentNode.x(), rect.ymin(), rect.xmax(), rect.ymax()));
 		}
 		else if (parentNode.y() > point.y()) { // Vertical below
-			node.lb = put(node.lb, point, val, orient, new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), parentNode.y()));
+			node.leftOrBottom = put(node.leftOrBottom, point, val, orient, new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), parentNode.y()));
 		}
 		else { // Vertical above
-			node.rb = put(node.rb, point, val, orient, new RectHV(rect.xmin(), parentNode.y(), rect.xmax(), rect.ymax()));
+			node.rightOrTop = put(node.rightOrTop, point, val, orient, new RectHV(rect.xmin(), parentNode.y(), rect.xmax(), rect.ymax()));
 		}
 		
 		N++;
@@ -114,7 +117,7 @@ public class KdTreeST<Value> {
 	 * @return
 	 */
 	public Value get(Point2D p) {
-		return get(root, p);
+		return get(root, p, true);
 	}
 	
 	/**
@@ -123,14 +126,24 @@ public class KdTreeST<Value> {
 	* @param point
 	* @return Value
 	*/
-	private Value get(Node node, Point2D point) {
-	        if (node == null) return null;
-	        
-	        int cmp = point.compareTo(node.p);
-	        
-	        if      (cmp < 0) return get(node.lb, point);
-	        else if (cmp > 0) return get(node.rb, point);
-	        else              return node.val;
+	private Value get(Node parentNode, Point2D point, boolean orient) {
+        if (parentNode == null) return null;
+
+        if (parentNode.p.compareTo(point) == 0) {
+        	return parentNode.val;
+        }
+        else if (orient && parentNode.p.x() > point.x()) { // Horizontal left
+        	return get(parentNode.leftOrBottom, point, !orient);
+		}
+		else if (orient) { // Horizontal right
+        	return get(parentNode.rightOrTop, point, !orient);
+		}
+		else if (!orient && parentNode.p.y() > point.y()) { // Vertical below
+        	return get(parentNode.leftOrBottom, point, !orient);
+		}
+		else { // Vertical above
+        	return get(parentNode.rightOrTop, point, !orient);
+		}
 	}
 	
 	/**
@@ -139,28 +152,71 @@ public class KdTreeST<Value> {
 	 * @return boolean
 	 */
 	public boolean contains(Point2D p) {
-		//TODO
-		return false;
+		return get(root, p, true) != null;
 	}
-	
+
 	/**
-	 * all points in the symbol table 
-	 * @return Iterable<Point2D>
-	 */
-	public Iterable<Point2D> points() {
-		//TODO:
-		return null;
-	}
+     * all points in the symbol table 
+     * @return Iterable<Point2D>
+     */
+    public Iterable<Point2D> points() {
+        List<Point2D> pointsList = new LinkedList<Point2D>();
+        Queue<Node> nodeQueue = new Queue<Node>();
+        
+        if(root == null) return pointsList;
+        
+        pointsList.add(root.p);
+        nodeQueue.enqueue(root);
+        
+        while(!nodeQueue.isEmpty()) {
+            Node currentNode = nodeQueue.dequeue();
+            
+            if(currentNode.leftOrBottom != null) {
+                pointsList.add(currentNode.leftOrBottom.p);
+                nodeQueue.enqueue(currentNode.leftOrBottom);
+            }
+            
+            if(currentNode.rightOrTop != null) {
+                pointsList.add(currentNode.rightOrTop.p);
+                nodeQueue.enqueue(currentNode.rightOrTop);
+            }
+        }
+        return pointsList;
+    }
+    
+    /**
+     * all points that are inside the rectangle 
+     * @param rect
+     * @return
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        List<Point2D> points = new LinkedList<Point2D>();
+        range(rect, root, points, true);    
+        return points;
+    }
+    
+ 
+    
+    /**
+     * Helper method for range.
+     * @param rect
+     * @param root2
+     * @param points
+     * @param b
+     * @return 
+     */
+    private void range(RectHV rect, Node node, List<Point2D> points, boolean orient) {    	
+        Point2D point = node.p;       
+        if(rect.contains(point))
+            points.add(point);
+        
+        if(node.leftOrBottom != null && node.leftOrBottom.rect.intersects(rect))
+            range(rect, node.leftOrBottom, points, !orient);
+        
+        if(node.rightOrTop != null && node.rightOrTop.rect.intersects(rect))
+            range(rect, node.rightOrTop, points, !orient);
+    }
 	
-	/**
-	 * all points that are inside the rectangle 
-	 * @param rect
-	 * @return
-	 */
-	public Iterable<Point2D> range(RectHV rect) {
-		//TODO
-		return null;
-	}
 	
 	/**
 	 * nearest neighbor to point p; null if the symbol table is empty 
@@ -168,7 +224,6 @@ public class KdTreeST<Value> {
 	 * @return
 	 */
 	public Point2D nearest(Point2D p) {
-		//TODO 
 		return null;
 	}
 	
@@ -184,12 +239,21 @@ public class KdTreeST<Value> {
 		Point2D p3 = new Point2D(0.2, 0.3);
 		myKdTree.put(p3, p3);
 		
-		Point2D p4 = new Point2D(0.4, 0.7);
+		Point2D p4 = new Point2D(0.4, 0.7);	
 		myKdTree.put(p4, p4);
 		
 		Point2D p5 = new Point2D(0.9, 0.6);
 		myKdTree.put(p5, p5);
 		
-		System.out.println("done");
+//		Point2D p6 = new Point2D(0.1, 0.9);
+//		Point2D p7 = new Point2D(0.3, 0.7);
+//		Point2D p8 = new Point2D(0.5, 0.5);
+//		Point2D p9 = new Point2D(0.7, 0.3);
+//		Point2D p10 = new Point2D(0.9, 0.1);
+		
+		RectHV rect = new RectHV(0.4, 0.2, 0.6, 0.7);
+		
+		System.out.println(myKdTree.range(rect));
+		
 	}
 }
